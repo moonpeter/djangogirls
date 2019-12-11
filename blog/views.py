73369008568ad74692b0462a9acb832cffa23cda
moonpeter <moonpeter@kakao.com>
2019-12-11 15:48:52 +0900

    template을 찾을 경로를 settings.TEMPLATE에 정하고, post_list view에서는 render()함수를 사용해서 템플릿 파일을 text로 변환하고 응답하도록 함

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,5 @@ def post_list(request):
     #  post_list.html을 찾아서
     #  그 파일을 text로 만들어서 HttpResponse형태로 돌려준다.
     # 위 기능을 하는 shortcut함
-    
 
     return render(request, 'post_list.html')
--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print('settings.py의 BASE_DIR:', BASE_DIR)
-TEMPLATES_DIR = os.path.join()
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 print('setting.py의 TEMPLATES_DIR:', TEMPLATES_DIR)
 
 
